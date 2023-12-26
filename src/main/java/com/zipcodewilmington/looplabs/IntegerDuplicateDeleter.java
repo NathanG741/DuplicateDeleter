dd8be6fdfb19c01d78eedddeeeeeb1f25360d602
NathanG741 <nathan.guardado@yahoo.com>
2023-12-26 18:56:03 -0500

    Finished

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -1,8 +1,67 @@
 package com.zipcodewilmington.looplabs;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by leon on 1/29/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
+
+    public IntegerDuplicateDeleter(Integer[] intArray) {
+        super(intArray);
+    }
+
+    @Override
+    public Integer[] removeDuplicates(int maxNumberOfDuplications) {
+        String str = countDuplicatesAndKeepInString(false, maxNumberOfDuplications);
+        return parseStringToIntArray(str);
+    }
+
+    @Override
+    public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        String str = countDuplicatesAndKeepInString(true, exactNumberOfDuplications);
+        return parseStringToIntArray(str);
+    }
+
+    private String countDuplicatesAndKeepInString(boolean isExact, int numberOfDuplications) {
+        String str = "";
+
+        for (int i = 0; i < this.array.length; i++) {
+            int count = 0;
+            for (int j = 0; j < this.array.length; j++) {
+                if (this.array[i].equals(this.array[j])) {
+                    count++;
+                }
+            }
+
+            if (!isExact) {
+                if (count < numberOfDuplications) {
+                    str += this.array[i];
+                }
+            } else if (isExact) {
+                if (count != numberOfDuplications) {
+                    str += this.array[i];
+                }
+            }
+        }
+
+        return str;
+    }
+
+    private static Integer[] parseStringToIntArray(String str) {
+        if (str.isEmpty()) {
+            return new Integer[]{};
+        } else {
+            String[] strArray = str.split("");
+            Integer[] intArray = new Integer[strArray.length];
+
+            for (int i = 0; i < strArray.length; i++) {
+                intArray[i] = Integer.parseInt(strArray[i]);
+            }
+
+            return intArray;
+        }
+    }
 }
--- src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
@@ -5,4 +5,48 @@ package com.zipcodewilmington.looplabs;
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
+
+    public StringDuplicateDeleter(String[] stringArray) {
+        super(stringArray);
+    }
+
+    @Override
+    public String[] removeDuplicates(int maxNumberOfDuplications) {
+        return countDuplicatesAndKeepInString(false, maxNumberOfDuplications);
+    }
+
+    @Override
+    public String[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        return countDuplicatesAndKeepInString(true, exactNumberOfDuplications);
+    }
+
+    private String[] countDuplicatesAndKeepInString(boolean isExact, int numberOfDuplications) {
+        String str = "";
+
+        for (int i = 0; i < this.array.length; i++) {
+            int count = 0;
+            for (int j = 0; j < this.array.length; j++) {
+                if (this.array[i].equals(this.array[j])) {
+                    count++;
+                }
+            }
+
+            if (!isExact) {
+                if (count < numberOfDuplications) {
+                    str += this.array[i] + "/";
+                }
+            } else if (isExact) {
+                if (count != numberOfDuplications) {
+                    str += this.array[i] + "/";
+                }
+            }
+        }
+
+        if (str.isEmpty()) {
+            return new String[]{};
+        } else {
+            return str.split("/");
+        }
+    }
+
 }
